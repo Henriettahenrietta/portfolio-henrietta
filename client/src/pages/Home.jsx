37074e4dc44c1ee53a8df8d2841436977abd78bd
Henrietta <henriettahenrietta322>
2026-02-17 18:10:38 +0100

    Added home page structure

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <section className="min-h-screen flex flex-col justify-center items-center bg-white text-black">
+      <h1 className="text-4xl font-bold">Henrietta Isange</h1>
+      <p className="text-lg mt-2 text-brownPrimary">
+        Full Stack Developer
+      </p>
+    </section>
+  );
+}
